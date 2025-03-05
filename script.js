a55d666c34679e3be9fe1a2281e844f3097f6cae
ShanteL Gray <Shantelgray24@gmail.com>
2025-03-05 16:52:08 -0500

    fixed up website

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
 const h2 = document.createElement("h2");
-h2.textContent = "This content added by JavaScript";
+h2.textContent = "Aspiring Coder";
 document.querySelector("body").appendChild(h2);
 
+const toggleButton = document.getElementById('toggleButton');
+const toggleContent = document.getElementById("toggleContent");
+
+toggleButton.addEventListener('click', function() {
+    if(toggleContent.style.display=== 'none') {
+        toggleContent.style.display = "block";
+    } else {
+        toggleContent.style.display = "none";
+    }
+});
